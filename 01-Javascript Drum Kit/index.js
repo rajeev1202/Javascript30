16e6416c25feec0a0a29e89d49ce56f5334cbbdb
Rajeev Ranjan <rajeev.ranjan@routerabbit.com>
2022-04-08 10:30:21 +0530

    01-JS drum kit - added transition and color and fix a issue when keys are pressed before transitionend event

--- 01-Javascript Drum Kit/index.js
+++ 01-Javascript Drum Kit/index.js
@@ -1,21 +1,48 @@
  window.addEventListener("keydown",function(e){
      onKeyPress(e)
  })
-function addCickEvent(){
+// function addCickEvent(){
    
-var keys = document.getElementsByClassName("key")
-Array.from(keys).forEach(elem => {
-    elem.addEventListener("click",onKeyPress)
-})
+// var keys = document.getElementsByClassName("key")
+// Array.from(keys).forEach(elem => {
+//     elem.addEventListener("click",onKeyPress)
+// })
+// }
+
+// function addTransition(keycode){
+// let keys = document.querySelectorAll("div.key")
+// Array.from(keys).forEach((e) =>{
+//     if(e.getAttribute("data-key") == keycode){
+//         e.classList.add("play-key")
+//     }
+
+// })
+// }
+
+function removeTransition(e){
+    if(e.propertyName != "transform") return
+    this.classList.remove("play-key")
 }
-
-
 
 function onKeyPress(e){
     let keyCode = e.keyCode
     let audio = document.querySelector(`audio[data-key="${keyCode}"]`)
+    let key = document.querySelector(`div[data-key="${keyCode}"]`)
     if(!audio) return;
     audio.currentTime = 0;
+    if(!audio) return
     audio.play()
+    key.classList.add("play-key")
+
+    if(document.querySelectorAll(".play-key").length >1){
+        document.querySelectorAll(".play-key").forEach(playKey =>{
+            if(! (playKey.getAttribute("data-key") == keyCode)){
+                playKey.classList.remove("play-key")
+            }
+        })
+    }
+    
+    key.addEventListener("transitionend",removeTransition)
+    
 }
 
